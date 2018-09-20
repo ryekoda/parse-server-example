ec9351fbe3ae643bf1bd51a221de27ed9f8f952a
Ryan <ryekoda@gmail.com>
2018-09-19 23:52:19 -0700

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://RyanMaurice:Simplery1@ds139884.mlab.com:39884/postupemailreferral',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
